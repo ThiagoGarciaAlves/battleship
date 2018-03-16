43b939a1fa05de432b0abbb4575ab58dfdc94d5e
Thiago G. Alves <thiagogarciaalves@mail.com>
2018-03-15 21:18:30 -0300

    Mudando o nome do pacote na classe de teste

--- src/main/java/br/fatecsp/engsw3/battleship/controller/IndexController.java
+++ src/main/java/br/fatecsp/engsw3/battleship/controller/IndexController.java
@@ -1,4 +1,4 @@
-package br.fatecsp.engsw3.battleship.control;
+package br.fatecsp.engsw3.battleship.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
